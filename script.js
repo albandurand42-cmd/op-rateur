bd0d39bd39213fa2f451158f206a3136bf8e1a52
albandurand42-cmd <albandurand42@gmail.com>
2026-08-12 15:23:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ setInterval(fetchWeather, OPEN_METEO.refreshInterval);
 
 // ----- Planning: connexion au Web App Google Apps Script fourni -----
 // Web App URL fourni par l'utilisateur (doit renvoyer le JSON décrit dans la demande)
-const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbxgSA42NHcJUTwB4EkUDW5833mRj9JuPpEONJH6I9WLQszhZRNtkRCXe7XQF1wq9IUQgg/exec';
+const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyKlwih9V9fVWsKa-jQlhgXQXSprEBbMaNBrs_K6Bv57CEXP9wjcA87Y3ezP31qkMo3Bg/exec';
 
 // cache des dernières données affichées : en cas d'erreur, on conserve l'affichage
 let cachedToday = null;
